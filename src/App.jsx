695905b1b596ddd3bd4ff3ea8d7c7c53a50436a1
rajeshsanaka <rajeshgroot@gmail.com>
2025-10-23 15:47:38 +0530

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Footer from "./comp/Footer";
 import LandingPage from "./comp/LandingPage";
 import Login from "./pages/Login";
 import { Routes,Route } from "react-router-dom";
-import Reg from "./pages/Reg";
 import Sample from "./pages/Sample";
 const App = () => {
   return (
